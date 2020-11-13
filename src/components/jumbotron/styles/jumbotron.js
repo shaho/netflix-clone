43680b4022c5c04dfb2ecf7ad3baedd72756edd9
Shaho <toofani@gmail.com>
2020-11-13 21:42:40 +0330

    Add jumbotron and images

--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -1,7 +1,53 @@
-/* eslint no-use-before-define: 0 */
-// import styled from "styled-components/macro";
-import styled from "styled-wind";
+// import styled from "styled-wind";
+import styled from "styled-components/macro";
+import tw from "tailwind.macro";
 
 export const Inner = styled.div`
-  .flex;
+  flex-direction: ${({ direction }) => direction};
+  ${tw`
+    flex
+    items-center
+    justify-between
+    m-auto
+    w-full
+
+    lg:flex-col
+  `};
+`;
+
+export const Pane = styled.div`
+  ${tw`
+    w-1/2
+
+  `}
+`;
+
+export const Container = styled.div`
+  ${tw`
+      
+  `}
+`;
+
+export const Title = styled.h1`
+  ${tw`
+      leading-none
+      sm:text-5xl
+  `}
+`;
+
+export const SubTitle = styled.h2`
+  ${tw`
+      text-lg
+      font-normal
+      leading-normal
+
+      sm:text-2xl
+  `}
+`;
+
+export const Image = styled.img`
+  ${tw`
+    max-w-full
+    h-auto
+  `}
 `;
